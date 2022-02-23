74874578cf695c7c12114711b20a2fa65dc43146
vattsalbhatt <vatsbhatt333@gmail.com>
2022-02-23 11:54:08 +0530

    making it better

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
+const port = process.env.PORT || 7896;
 
 const connect = require("./configs/db");
 const categoryController = require("./controllers/category.controller");
@@ -9,10 +10,13 @@ const productController = require("./controllers/product.controller");
 app.use("/categories", categoryController);
 app.use("/products", productController);
 
-app.listen(7896, async (req, res) => {
+app.listen(port, async (req, res) => {
   try {
     await connect();
-    console.log("Listening to PORT 7896 steve_madden project");
+    console.log(`Listening to PORT ${port} steve_madden project`);
+    // return res
+    //   .status(200)
+    //   .send(`Listening to PORT ${port} steve_madden project`);
   } catch (e) {
     console.log(e.message);
   }
