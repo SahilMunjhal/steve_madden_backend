d0ac57c9ef9d7aeb6074e4ba2af39193234ed37b
vattsalbhatt <vatsbhatt333@gmail.com>
2022-02-26 18:38:00 +0530

    final

--- src/controllers/cart.controller.js
+++ src/controllers/cart.controller.js
@@ -1,52 +1,71 @@
-const express=require("express");
-const router=express.Router();
-const Cart=require("../models/cartmodel");
+const express = require("express");
+const app = express();
+const router = express.Router();
+const Cart = require("../models/cartmodel");
 
-router.post("",async(req,res)=>{
-    try{
-        console.log(req.body);
-        let cart=await Cart.create(req.body);
-        console.log(cart);
-        return res.status(205).send(cart);
-    }catch(err){
-        return res.status(505).send(err.message);
-    }
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET",
+    "PUT",
+    "POST",
+    "DELETE",
+    "OPTIONS"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
 });
 
-router.get("",async(req,res)=>{
-    try{
-        const carts=await Cart.find().lean().exec();
-        return res.status(206).send(carts);
-    }catch(err){
-        return res.status(506).send(err.message);
-    }
+router.post("", async (req, res) => {
+  try {
+    console.log(req.body);
+    let cart = await Cart.create(req.body);
+    console.log(cart);
+    return res.status(205).send(cart);
+  } catch (err) {
+    return res.status(505).send(err.message);
+  }
 });
 
-router.get("",async(req,res)=>{
-    try{
-        const cart=await Cart.findOne().lean().exec();
-        return res.status(206).send(cart);
-    }catch(err){
-        return res.status(506).send(err.message);
-    }
+router.get("", async (req, res) => {
+  try {
+    const carts = await Cart.find().lean().exec();
+    return res.status(206).send(carts);
+  } catch (err) {
+    return res.status(506).send(err.message);
+  }
 });
 
-router.get("/:id",async(req,res)=>{
-    try{
-        const cart=await Cart.findOne(req.params.id).lean().exec();
-        return res.status(206).send(cart);
-    }catch(err){
-        return res.status(506).send(err.message);
-    }
+router.get("/:user", async (req, res) => {
+  try {
+    const cart = await Cart.find({ user_id: req.params.user }).lean().exec();
+    return res.status(206).send(cart);
+  } catch (err) {
+    return res.status(506).send(err.message);
+  }
 });
 
-router.delete("/:id",async(req,res)=>{
-    try{
-        const cart=await Cart.findByIdAndDelete(req.params.id).lean().exec();
-        return res.status(206).send(cart);
-    }catch(err){
-        return res.status(506).send(err.message);
-    }
+router.get("/:id", async (req, res) => {
+  try {
+    const cart = await Cart.findById(req.params.id).lean().exec();
+    return res.status(206).send(cart);
+  } catch (err) {
+    return res.status(506).send(err.message);
+  }
 });
 
-module.exports=router;
+router.delete("/:id", async (req, res) => {
+  try {
+    console.log("yes");
+    const cart = await Cart.findByIdAndDelete(req.params.id);
+    return res.status(200).send(cart);
+  } catch (err) {
+    return res.status(506).send(err.message);
+  }
+});
+
+module.exports = router;
